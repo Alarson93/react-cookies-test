79efcb7b681cea9addcba1bd13aa79d087c3d4e4
Alex Larson <alarson@bushelpowered.com>
2024-02-16 12:19:14 -0600

    Basic cookie create and delete buttons.

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { destroyCookie, setCookie } from "nookies";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div></div>
+
+      <div className={styles.buttons}>
+        <button onClick={() => createCookie()}>Create Cookie</button>
+        <button onClick={() => deleteCookie()}>Destroy Cookie</button>
+      </div>
+
+      <div></div>
+    </main>
+  );
+}
+
+function createCookie() {
+  setCookie(null, "myCookie", Math.random().toString());
+}
+
+function deleteCookie() {
+  destroyCookie(null, "myCookie");
+}
